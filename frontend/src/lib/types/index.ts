823e8806571d2b940e2da98f94a90daa256107a1
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-04 03:57:13 +0200

    feat: add types barrel export file

---
+++ frontend/src/lib/types/index.ts
@@ -0,0 +1,5 @@
+export type { User } from './user';
+export type { AttendanceRecord } from './attendance';
+export type { AttendanceHistoryRecord } from './attendanceHistory';
+export type { UnexcusedAbsence } from './unexcusedAbsence';
+export type { AbsenceRecord } from './absence';
