4d0d86894e4e7db907a0e3eff779e443f17e9a23
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-04 03:38:32 +0200

    Add CameraContainer component

---
+++ frontend/src/app/camera/views/containers/CameraContainer.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { useCamera } from "../../hooks/useCamera";
+import { useAttendance } from "../../hooks/useAttendance";
+import { useCameraHandlers } from "../../hooks/useCameraHandlers";
+import { CameraLayout, CameraContent, CameraModals } from "./components";
+
+export default function CameraContainer({ mode }: { mode: string }) {
+  const { cameraActive, isProcessing, videoRef, canvasRef, startCamera, stopCamera, captureImage } = useCamera();
+  const { 
+    attendanceMarked, 
+    recognizedUser,
+    error, 
+    attemptsRemaining, 
+    exhaustedAttempts, 
+    multipleFaces, 
+    detecting,
+    showAlreadyTakenModal,
+    checkedOut,
+    checkOutData,
+    verificationStep,
+    faceStatus,
+    idStatus,
+    recognizedName,
+    verifiedId,
+    showUnauthorizedWarning,
+    recognizedImage,
+    detectedUser,
+    expectedUser,
+    setShowAlreadyTakenModal,
+    processAttendance,
+    processCheckOut, 
+    resetState, 
+    setError 
+  } = useAttendance();
+
+  const { handleStartCamera, handleCaptureAndDetect, handleCheckOut, handleRetry } = useCameraHandlers({
+    startCamera,
+    stopCamera,
+    captureImage,
+    processAttendance,
+    processCheckOut,
+    resetState,
+    setError,
+    cameraActive,
+    videoRef
+  });
+
+  return (
+    <CameraLayout>
+      <CameraContent
+        ref={videoRef}
+        cameraActive={cameraActive}
+        isProcessing={isProcessing}
+        attendanceMarked={attendanceMarked}
+        recognizedUser={recognizedUser}
+        recognizedEmployee={recognizedUser}
+        error={error}
+        attemptsRemaining={attemptsRemaining}
+        exhaustedAttempts={exhaustedAttempts}
+        multipleFaces={multipleFaces}
+        detecting={detecting}
+        mode={mode}
+        checkedOut={checkedOut}
+        checkOutData={checkOutData}
+        verificationStep={verificationStep}
+        faceStatus={faceStatus}
+        idStatus={idStatus}
+        recognizedName={recognizedName}
+        verifiedId={verifiedId}
+        showUnauthorizedWarning={showUnauthorizedWarning}
+        recognizedImage={recognizedImage}
+        expectedUser={expectedUser}
+        detectedUser={detectedUser}
+        onStartCamera={handleStartCamera}
+        onCaptureAndDetect={handleCaptureAndDetect}
+        onCheckOut={handleCheckOut}
+        onRetry={handleRetry}
+      />
+      
+      <canvas ref={canvasRef} className="hidden" />
+      
+      <CameraModals
+        showAlreadyTakenModal={showAlreadyTakenModal}
+        setShowAlreadyTakenModal={setShowAlreadyTakenModal}
+      />
+    </CameraLayout>
+  );
+}
