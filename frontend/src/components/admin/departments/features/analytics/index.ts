8502f055da3743eab2fd8c19dd93ff6ceaf7be9f
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-04 04:21:08 +0200

    Add analytics index file

---
+++ frontend/src/components/admin/departments/features/analytics/index.ts
@@ -0,0 +1,5 @@
+export { default as SummaryCards } from './cards/SummaryCards';
+export { default as DepartmentBreakdown } from './breakdown';
+export { default as LoadingState } from './states/LoadingState';
+export { default as EmptyState } from './states/EmptyState';
+export { useDepartmentAnalytics } from '../../hooks';
