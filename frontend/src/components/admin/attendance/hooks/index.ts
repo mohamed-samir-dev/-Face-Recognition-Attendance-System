56c0d3bb076130cfb5429ffd099cf45d30a7b1d9
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-04 04:10:00 +0200

    Add hooks index exports

---
+++ frontend/src/components/admin/attendance/hooks/index.ts
@@ -0,0 +1,6 @@
+export { useAttendanceData } from './data/useAttendanceData';
+export { useLeaveRequests } from './data/useLeaveRequests';
+export { useFilteredRequests } from './data/useFilteredRequests';
+export { useModalState } from './ui/useModalState';
+export { useTabCounts } from './ui/useTabCounts';
+export { useLeaveActions } from './actions/useLeaveActions';
