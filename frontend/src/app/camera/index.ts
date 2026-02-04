8416e12df9a03cdf9f4a58aa3c186e88d9e06c79
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-04 03:20:10 +0200

    Add camera module index

---
+++ frontend/src/app/camera/index.ts
@@ -0,0 +1,13 @@
+// Views
+export * from "./views";
+
+// Components
+export * from "./components";
+
+
+// Types
+export * from "./types";
+
+// Legacy exports for backward compatibility
+export { CameraPreview, CameraControls } from "./components";
+export { CameraView as CameraContainer } from "./views";
