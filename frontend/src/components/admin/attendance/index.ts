4b0e461215d1a80afcf72664f5b22232d65616d7
Mohamed Samir Mouawad <114841411+mohamed-samir-dev@users.noreply.github.com>
2026-02-04 04:08:24 +0200

    Add attendance module index exports

---
+++ frontend/src/components/admin/attendance/index.ts
@@ -0,0 +1,22 @@
+// Content Components
+export { default as AttendanceContent } from './content/AttendanceContent';
+export { default as LeavesContent } from './content/LeavesContent';
+
+// Overview Components
+export { default as AttendanceOverview } from './overview/AttendanceOverview';
+
+// Actions
+export { default as QuickActions } from './actions/QuickActions';
+
+// Reasons
+export { default as AbsenceReasons } from './reasons/AbsenceReasons';
+
+// Status Components
+export { default as AttendanceStatus } from './status/components/AttendanceStatus';
+export { default as DepartmentAttendance } from './status/components/DepartmentAttendance';
+
+// Hooks
+export * from './hooks';
+
+// Types
+export * from './types';
